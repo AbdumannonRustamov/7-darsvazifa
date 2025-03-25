f3b2149a3b83c40f1f47c15a1e1f96db15c3a125
m <m>
2025-03-26 00:23:03 +0500

    h

---
+++ src/components/EmptyCard.jsx
@@ -0,0 +1,11 @@
+function EmptyCard() {
+  return (
+    <div className="your-card">
+    <span className="text">Your Card(0)</span>
+    <img className="deffault-img" src="../public/images/deffault.png" alt="" />
+    <p className="p-text">Your added items will appear here</p>
+  </div>
+  )
+}
+
+export default EmptyCard
--- src/components/YourCard.jsx
+++ src/components/YourCard.jsx
@@ -1,11 +1,5 @@
 function YourCard() {
   return (
-    // <div className="your-card">
-    //   <span className="text">Your Card(0)</span>
-    //   <img className="deffault-img" src="../public/images/deffault.png" alt="" />
-    //   <p className="p-text">Your added items will appear here</p>
-    // </div>
-
     <div className="your-card-2">
       <span className="text-2">Your Card(0)</span>
       <div className="wrap-card">
@@ -19,9 +13,52 @@ function YourCard() {
         </div>
         <div>
         <button className="delete-button">x</button>
-
         </div>
       </div>
+
+        <p className="border"></p>
+
+      <div className="wrap-card">
+        <div>
+          <div className="product-name">
+            <p>Classic Tiramisu</p>
+          </div>
+          <div className="product-price">
+            <p className="p-color">1x</p> <p>@$5.50</p> <p>$5.50</p>
+          </div>
+        </div>
+        <div>
+        <button className="delete-button">x</button>
+        </div>
+      </div>
+
+      <p className="border"></p>
+
+       <div className="wrap-card">
+        <div>
+          <div className="product-name">
+            <p>Classic Tiramisu</p>
+          </div>
+          <div className="product-price">
+            <p className="p-color">1x</p> <p>@$5.50</p> <p>$5.50</p>
+          </div>
+        </div>
+        <div>
+        <button className="delete-button">x</button>
+        </div>
+      </div>
+
+      <p className="border"></p>
+
+      <div>
+        <div className="order-total">
+          <p className="text-order">Order Total</p>
+          <h2 className="order-price">$46.50</h2>
+        </div>
+        <button className="btn-1">This is a carbon-neutral  delivery</button>
+        <button className="btn-2">Confirm Order</button>
+      </div>
+
     </div>
   );
 }
