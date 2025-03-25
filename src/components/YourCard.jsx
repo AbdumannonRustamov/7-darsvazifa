0ed099560a4227386c8635bae34430f4dbc6e805
m <m>
2025-03-25 23:32:20 +0500

    qoshdim

--- src/components/YourCard.jsx
+++ src/components/YourCard.jsx
@@ -1,8 +1,29 @@
-
 function YourCard() {
   return (
-    <div>YourCard</div>
-  )
+    // <div className="your-card">
+    //   <span className="text">Your Card(0)</span>
+    //   <img className="deffault-img" src="../public/images/deffault.png" alt="" />
+    //   <p className="p-text">Your added items will appear here</p>
+    // </div>
+
+    <div className="your-card-2">
+      <span className="text-2">Your Card(0)</span>
+      <div className="wrap-card">
+        <div>
+          <div className="product-name">
+            <p>Classic Tiramisu</p>
+          </div>
+          <div className="product-price">
+            <p className="p-color">1x</p> <p>@$5.50</p> <p>$5.50</p>
+          </div>
+        </div>
+        <div>
+        <button className="delete-button">x</button>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default YourCard
+export default YourCard;
